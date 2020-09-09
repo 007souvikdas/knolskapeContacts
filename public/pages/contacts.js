6490bb096ab574e9df71bc260adb86b3e561496a
root <souvik@paisabazaar.com>
2020-09-09 18:41:03 +0530

    Adding logout button

--- public/pages/contacts.js
+++ public/pages/contacts.js
@@ -1,34 +1,49 @@
-window.onload = (event) => {
-    getContacts();
-}
+const logoutButton = document.getElementById('logout');
 
-function getContacts() {
-    let contactsDiv = document.getElementById("contactsDiv");
-    var xhttp = new XMLHttpRequest();
+logoutButton.onclick = (e) => {
+    console.log('logging out');
+
+    const xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState === XMLHttpRequest.DONE) {
+            console.log(xhttp.response);
+            window.location.href = '/knol/pages/login';
+        }
+    };
+    xhttp.open('GET', '/knol/api/v1/user/logout', true);
+    xhttp.send();
+};
 
+window.onload = (event) => {
+    getContacts();
+};
+
+function getContacts() {
+    const contactsDiv = document.getElementById('contactsDiv');
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState === XMLHttpRequest.DONE) {
             if (xhttp.status == 200) {
-                let res = JSON.parse(xhttp.response);
-                for (let individualContact of res) {
+                const res = JSON.parse(xhttp.response);
+                for (const individualContact of res) {
+                    const contactDiv = document.createElement('div');
 
-                    let contactDiv = document.createElement('div');
-
-                    let img = document.createElement('img');
+                    const img = document.createElement('img');
                     img.src = individualContact.photoUrl;
                     contactDiv.appendChild(img);
 
-                    let name = document.createElement('p');
+                    const name = document.createElement('p');
                     name.innerText = individualContact.name;
                     contactDiv.appendChild(name);
 
-                    let phoneNo = document.createElement('p');
+                    const phoneNo = document.createElement('p');
                     phoneNo.innerText = individualContact.phoneNumber;
                     contactDiv.appendChild(phoneNo);
 
-                    let emailAddress = document.createElement('p');
-                    emailAddress.innerText = individualContact.emailAddress || "-";
+                    const emailAddress = document.createElement('p');
+                    emailAddress.innerText = individualContact.emailAddress || '-';
                     contactDiv.appendChild(emailAddress);
 
                     contactsDiv.appendChild(contactDiv);
@@ -38,6 +53,6 @@ function getContacts() {
             }
         }
     };
-    xhttp.open("GET", `/knol/api/v1/contacts`, true);
+    xhttp.open('GET', '/knol/api/v1/contacts', true);
     xhttp.send();
 }
